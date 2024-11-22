3ed76c06088fcd12d1d216f0955d6c67a63e238f
Megha Prajapati <mp348730@gmail.com>
2024-11-22 12:05:14 +0530

    Exercises 1.3-1.4 already done, Exercise 1.5 completed

--- part1/course_info/src/App.jsx
+++ part1/course_info/src/App.jsx
@@ -23,28 +23,30 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
+  let total = 0;
+  props.parts.forEach((part) => {
+    total += part.exercises
+  })
   return (
-    <p>Number of exercises {props.total}</p>
+    <p>Number of exercises {total}</p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts =[
-    {name: 'Fundamentals of React', exercises: 10},
-    {name: 'Using props to pass data', exercises: 7},
-    {name: 'State of a component', exercises: 14}
-  ]
-  var total = 0
-  parts.forEach((part) => {
-    total += part.exercises
-  })
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {name: 'Fundamentals of React', exercises: 10},
+      {name: 'Using props to pass data', exercises: 7},
+      {name: 'State of a component', exercises: 14}
+    ]
+  } 
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total total={total} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
