408dca9d5af882715aa9e1a80a1d32a5c8f88e0e
Megha Prajapati <mp348730@gmail.com>
2024-11-23 16:08:50 +0530

    Exercise 1.7 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,17 +5,40 @@ const App = () => {
   const [ good, setGood ] = useState(0);
   const [ neutral, setNeutral ] = useState(0);
   const [ bad, setBad ] = useState(0);
+  const [ total, setTotal ] = useState(0);
 
   const handleGood = () => {
-    setGood(good+1);
+    const updatedGood = good + 1;
+    setGood(updatedGood);
+    setTotal(updatedGood+neutral+bad);
   };
 
   const handleNeutral = () => {
-    setNeutral(neutral+1);
+    const updatedNeutral = neutral + 1;
+    setNeutral(updatedNeutral);
+    setTotal(good+updatedNeutral+bad);
   };
 
   const handleBad = () => {
-    setBad(bad+1);
+    const updatedBad = bad + 1;
+    setBad(updatedBad);
+    setTotal(good+neutral+updatedBad);
+  };
+
+  const calculateAverage = () => {
+    if(total === 0)
+    {
+      return 0;
+    }
+    return (good - bad) / total;
+  };
+
+  const calculatePositive = () => {
+    if(total === 0)
+    {
+      return 0;
+    }
+    return (good / total) * 100;
   };
 
   return (
@@ -31,6 +54,9 @@ const App = () => {
         <p>Good {good}</p>
         <p>Neutral {neutral}</p>
         <p>Bad {bad}</p>
+        <p>Total {total}</p>
+        <p>Average {calculateAverage()}</p>
+        <p>Positive {calculatePositive()} %</p>
       </div>
     </div>
   );
