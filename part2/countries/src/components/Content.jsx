d29ae99e9989aaf8ec5cff0b5fdda013a4a5b69a
Megha Prajapati <mp348730@gmail.com>
2024-11-27 16:10:01 +0530

    Exercise 2.19 completed

--- part2/countries/src/components/Content.jsx
+++ part2/countries/src/components/Content.jsx
@@ -1,34 +1,54 @@
 import Display from './Display';
+import axios from 'axios';
+import { useState } from 'react';
 
 const Content = ({ list }) => {
 
-    if(list === null)
+    const [ selectedCountry, setSelectedCountry ] = useState(null);
+
+    const handleClick = (country) => {
+      axios
+        .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${country}`)
+        .then(response => {
+          setSelectedCountry(response.data)
+        });
+    }
+  
+    if(selectedCountry)
     {
+      return (
+        <Display country={selectedCountry} />
+      );
+    }
+    else
+    {
+      if(list === null)
+      {
+        return null;
+      }
+      
+      if(list.length > 10)
+      {
+        return (
+          <p>Too many matches, specify another filter.</p>
+        );
+      }
+      else if(list.length > 1)
+      {
+        return (
+          <>
+            {list.map((country, index) => <p key={index}>{country.name.common} <button onClick={() => {handleClick(country.name.common)}}>Show</button></p>)}
+          </>
+        )
+      }
+      else if(list.length == 1)
+      {
+        return (
+          <Display country={list[0]}/>
+        );
+      }
       return null;
     }
-    
-    if(list.length > 10)
-    {
-      return (
-        <p>Too many matches, specify another filter.</p>
-      );
-    }
-    else if(list.length > 1)
-    {
-      return (
-        <>
-          {list.map((country, index) => <p key={index}>{country.name.common}</p>)}
-        </>
-      )
-    }
-    else if(list.length == 1)
-    {
-      return (
-        <Display country={list[0]}/>
-      );
-    }
-    
-    return null;
   
   }
 
