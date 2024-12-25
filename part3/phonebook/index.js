5df699d70c81951012e35d3df87d5c8bf89ea429
Megha Prajapati <mp348730@gmail.com>
2024-12-25 09:58:10 +0530

    Exercise 3.4 completed

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const persons = [
+let persons = [
     {
         'id': '1',
         'name': 'Arto Hellas',
@@ -45,6 +45,12 @@ app.get('/api/persons/:id', (request, response) => {
 app.get('/info', (request, response) => {
     const time = new Date();
     response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${time}</p>`);
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    persons = persons.filter(person => person.id !== id);
+    response.status(204).end();
 });
 
 const PORT = 3001;
